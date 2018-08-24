3ff730a2ebc9f31f42bd1907cfba0c00a8951c39
再见田园犬 <tcstory@163.com>
2018-08-24 22:12:22 +0800

    添加了cors,并且把项目重命名为jungle-next-bk

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
 const qs = require('koa-qs');
 const _logger = require('koa-pino-logger');
 
@@ -9,6 +10,7 @@ const router = require('./router');
 
 const app = qs(new Koa());
 
+app.use(cors());
 app.use(bodyParser());
 
 app.use(_logger({

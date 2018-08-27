7ff7eed610d1d7c69a69de414d7d66045a20a100
再见田园犬 <tcstory@163.com>
2018-08-27 10:42:18 +0800

    [response.js] 调整了一下makeErrRes的逻辑

--- app/libs/response.js
+++ app/libs/response.js
@@ -1,34 +1,46 @@
 const {SUCCESS, NORMAL_ERROR, APP_NAME} = require('../constants');
 
 function makeRes(ctx, body = {}, method = 'undefined') {
-    ctx.body = {
-        code: SUCCESS,
-        app: APP_NAME,
-        method,
-        data: body,
-    }
+  let obj = {
+    msg: '',
+  };
+
+  if (typeof body === 'string') {
+    obj.msg = body;
+  } else {
+    obj = body;
+  }
+
+  ctx.body = {
+    code: SUCCESS,
+    app: APP_NAME,
+    method,
+    data: obj,
+  }
 }
 
 function makeErrRes(ctx, error, method = 'undefined') {
-    let msg = '';
+  let obj = {
+    msg: '',
+  };
 
-    if (typeof error.toString === 'function') {
-        msg = error.toString();
-    } else {
-        msg = error;
-    }
+  if (typeof error === 'string') {
+    obj.msg = error;
+  } else if (typeof error.toString === 'function') {
+    obj.msg = error.toString();
+  } else {
+    obj = error;
+  }
 
-    ctx.body = {
-        error: {
-            msg,
-        },
-        code: NORMAL_ERROR,
-        app: APP_NAME,
-        method,
-    }
+  ctx.body = {
+    error: obj,
+    code: NORMAL_ERROR,
+    app: APP_NAME,
+    method,
+  }
 }
 
 module.exports = {
-    makeRes,
-    makeErrRes,
+  makeRes,
+  makeErrRes,
 };
